587156e890c3ff585ec9d4235843ec9188d69964
Dip Dutt <emduttt456@gmail.com>
2024-05-20 20:34:14 +0600

    Initial Commit

---
+++ OnboardingView/OnboardingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewApp.swift
+//  OnboardingView
+//
+//  Created by Dip Dutt on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
